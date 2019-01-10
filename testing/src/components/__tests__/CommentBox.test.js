8f28f9b37d0ced357f350b089990aaa7981dd78a
nonameb3 <eepw0211@gmail.com>
2019-01-10 19:08:09 +0700

    add describe

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -8,7 +8,40 @@ beforeEach(()=>{
   wrapped = mount(<CommentBox/>);
 });
 
+// clear fake js div
+afterEach(()=>{
+  wrapped.unmount();
+});
+
+// check component has textarea and button
 it('Has comment box and a button',()=>{
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
+});
+
+describe('the text aera',()=>{
+  beforeEach(()=>{
+    // type 'new comment' to text aera
+    wrapped.find('textarea').simulate('change',{
+      target:{ value:'new comment'}
+    });
+    // force update component
+    wrapped.update();
+  });
+
+  // simulate user can type on text box
+  it('has a text aera that user can type in',()=>{
+    // check new value
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+  });
+
+  // simulate submit form, textarea is empty
+  it('has user can submit form',()=>{
+    // foce click submit
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
+
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+  });
+
 });
