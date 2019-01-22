178d33bb35ed05246cbb78b1ccac29af6073e015
nonameb3 <eepw0211@gmail.com>
2019-01-22 10:46:34 +0700

    finish auth project

--- auth/client/src/components/Header.js
+++ auth/client/src/components/Header.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import './HeaderStyle.css'
 
 export class Header extends Component {
+  renderHeader(){
+    if(this.props.authentication){
+      return(
+        <div>
+          <Link to="/signout">Sign Out</Link>
+          <Link to ="/feature">Feature</Link>
+        </div>
+      )
+    }else{
+      return(
+        <div>
+          <Link to="/signin">Sign In</Link>
+          <Link to="/signup">Sing Up</Link>
+        </div>
+      )
+    }
+  }
   render() {
     return (
-      <div>
-        Link
-        <Link to="/reduxauth">Redux Auth</Link>
-        <Link to="/signin">Sign In</Link>
-        <Link to="/signup">Sing Up</Link>
-        <Link to="/signout">Sign Out</Link>
-        <Link to ="/feature">Feature</Link>
+      <div className="header">
+        <Link to="/">Redux Auth</Link>
+        {this.renderHeader()}
       </div>
     )
   }
 }
 
-export default Header
+function MapStateToPros(state){
+  return {authentication: state.auth.authentication}
+}
+
+export default connect(MapStateToPros)(Header)
